d97fa5b20c2900c03ebbce9ba2c9738948feb15e
kaybrian <kayonbrian@gmail.com>
2020-06-26 00:27:17 +0300

    this is the index page

--- djtutor/settings.py
+++ djtutor/settings.py
@@ -14,6 +14,9 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+TEMPLATES_DIRS = os.path.join(BASE_DIR,'templates')
+STATIC_DIRS = os.path.join(BASE_DIR,'static')
+
 
 
 # Quick-start development settings - unsuitable for production
@@ -31,6 +34,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+
+    # Custom apps 
+    'polls',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
