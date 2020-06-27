46c06c860a02116f349b703061984f45167ab178
kaybrian <kayonbrian@gmail.com>
2020-06-27 16:41:05 +0300

    writing tests

--- djtutor/settings.py
+++ djtutor/settings.py
@@ -112,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'GMT'
+TIME_ZONE = 'Africa/Kampala'
 
 USE_I18N = True
 
--- polls/views.py
+++ polls/views.py
@@ -3,14 +3,18 @@ from django.http import Http404
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Question, Choice
 from django.urls import reverse
-from django.views import generic 
+from django.utils import timezone
+from django.views import generic
+
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(
+            pub_date__lte=timezone.now()
+        ).order_by('-pub_date')[:5]
 
 
 class DetailView(generic.DetailView):
@@ -19,10 +23,8 @@ class DetailView(generic.DetailView):
 
 
 class ResultsView(generic.DetailView):
-    model = Question 
+    model = Question
     template_name = 'polls/result.html'
-
-
 
 
 def vote(request, pk):
